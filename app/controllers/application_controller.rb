0f7a73206e61238ca3ca1e18239ccaf967579ae3
dominicod <dominicodonnell99@gmail.com>
2022-10-17 20:31:28 -0500

    Feat: begin process on customer error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def render_unprocessable_entity_response(exception)
+    render json: catch_exception(exception), status: :unprocessable_entity
+  end
+
+  def render_not_found_response(exception)
+    render json: catch_exception(exception), status: :not_found
+  end
+
+  private
+
+  def catch_exception(exception)
+    binding.pry
+  end
 end
