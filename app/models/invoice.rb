df5facae43f4c932e8dc51edfb40acc9fb8b1d9f
dominicod <dominicodonnell99@gmail.com>
2022-10-18 19:23:10 -0500

    Fix: quick patch to invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,7 +5,7 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
 
   validates :customer_id, presence: true
   validates :status, presence: true
