c6f2e9947924ad7f6dad93b2c27db31eabb21587
dominicod <dominicodonnell99@gmail.com>
2022-10-20 15:04:37 -0500

    Feat: create basic logic inside controller

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,5 +1,28 @@
-class FindController < ApplicationController
-  def index
+# frozen_string_literal: true
 
+module Api
+  module V1
+    module Merchants
+      class FindController < ApplicationController
+        def index
+          merchant = Merchant.find_by_name(query_params)
+          if merchant.nil?
+            render json: empty_hash
+          else
+            render json: MerchantSerializer.new(merchant)
+          end
+        end
+
+        private
+
+        def empty_hash
+          { data: {} }
+        end
+
+        def query_params
+          params.require(:name)
+        end
+      end
+    end
   end
 end
--- app/controllers/api/v1/merchants/find_all_controller.rb
+++ app/controllers/api/v1/merchants/find_all_controller.rb
@@ -1,5 +1,29 @@
-class FindAllController < ApplicationController
-  def index
+# frozen_string_literal: true
 
+module Api
+  module V1
+    module Items
+      class FindAllController < ApplicationController
+        def index
+          merchants = Merchant.find_all(query_params)
+          if merchants.nil? || merchants.empty?
+            render json: empty_arr
+          else
+            render json: MerchantSerializer.new(merchants)
+          end
+        end
+
+        private
+
+        def empty_arr
+          { data: [] }
+        end
+
+        def query_params
+          binding.pry
+          params.require(:name)
+        end
+      end
+    end
   end
 end
